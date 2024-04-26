9d64b4a1e522a614bd37bc531bfff6e19319a06b
Muideen7 <Olayeyeayomide2000@gmail.com>
2024-04-26 23:49:36 +0100

    benefits.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,20 +20,17 @@ const Hero = () => {
       id="hero"
     >
       <div className="container relative" ref={parallaxRef}>
-        <div
-          className="relative z-1 max-[62rem] 
-        mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]"
-        >
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
                 src={curve}
-                alt="curve"
                 className="absolute top-full left-0 w-full xl:mt-2"
                 width={624}
                 height={28}
+                alt="Curve"
               />
             </span>
           </h1>
