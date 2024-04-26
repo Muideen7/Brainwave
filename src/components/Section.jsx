88055d6ea59aa9d40172db0f5a41c07432c3286d
Muideen7 <Olayeyeayomide2000@gmail.com>
2024-04-26 13:47:44 +0100

    update_hero

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -11,12 +11,11 @@ const Section = ({
   return (
     <div
       id={id}
-      className={`relative ${
-        customPaddings ||
-        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
-          className || ""
-        }`
-      }`}
+      className={`
+      relative 
+      ${customPaddings ? customPaddings : "py-10 lg:py-16 xl:py-20"} 
+      ${crosses ? "lg:py-32 xl:py-40" : ""} 
+      ${className || ""}`}
     >
       {children}
       <div
@@ -26,8 +25,8 @@ const Section = ({
       />
       <div
         className="hidden absolute top-0 right-5 w-0.25 h-full 
-      bg-stroke-1 pointer-events-none md:block 
-      lg:right-7.5 xl:right-10"
+      bg-stroke-1 pointer-events-none 
+      md:block lg:right-7.5 xl:right-10"
       />
       {crosses && (
         <>
