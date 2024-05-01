84496eaf3f6fee0007f36fe1d093f779e2a047a2
Muideen7 <Olayeyeayomide2000@gmail.com>
2024-05-01 23:24:41 +0100

    add_getDate

--- src/constants/index.js
+++ src/constants/index.js
@@ -90,12 +90,18 @@ export const brainwaveServicesIcons = [
   chromecast,
   sliders04,
 ];
+
+const currentDate = new Date().toLocaleString("en-US", {
+  month: "long",
+  year: "numeric",
+});
+
 export const roadmap = [
   {
     id: "0",
     title: "Voice recognition",
     text: "Enable the chatbot to understand and respond to voice commands, making it easier for users to interact with the app hands-free.",
-    date: "May 2023",
+    date: currentDate,
     status: "done",
     imageUrl: roadmap1,
     colorful: true,
@@ -104,7 +110,7 @@ export const roadmap = [
     id: "1",
     title: "Gamification",
     text: "Add game-like elements, such as badges or leaderboards, to incentivize users to engage with the chatbot more frequently.",
-    date: "May 2023",
+    date: currentDate,
     status: "progress",
     imageUrl: roadmap2,
   },
@@ -112,7 +118,7 @@ export const roadmap = [
     id: "2",
     title: "Chatbot customization",
     text: "Allow users to customize the chatbot's appearance and behavior, making it more engaging and fun to interact with.",
-    date: "May 2023",
+    date: currentDate,
     status: "done",
     imageUrl: roadmap3,
   },
@@ -120,7 +126,7 @@ export const roadmap = [
     id: "3",
     title: "Integration with APIs",
     text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
-    date: "May 2023",
+    date: currentDate,
     status: "progress",
     imageUrl: roadmap4,
   },
